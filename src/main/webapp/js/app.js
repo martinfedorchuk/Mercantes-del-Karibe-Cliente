659e42f46b7263bdfb7849b55ba0e02c60cb4ba5
Alex Kalayjian <alexkalayjian@gmail.com>
2016-02-24 17:14:51 -0300

    Agrega assets bullet y missile - WS andando - Primera versión de disparos

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -12,6 +12,14 @@ var appJs = (function  () {
     xBottomRight: 5000,
     yBottomRight: 5000
   };
+
+  var bullet;
+  var bulletButton = null;
+  var fireRateBullet = 500;
+  var nextFire = 0;
+  var missile;
+  var missileButton = null;
+
   var newYork;
   var montevideo;
   var caribbean;
@@ -39,7 +47,8 @@ var appJs = (function  () {
     game.load.image('submarine', 'assets/ship-grey.png');
     game.load.image('red', 'assets/ship-red.png');
     game.load.image('island', 'assets/pattern-island.png');
-    game.load.image('mask', 'assets/mask.png');
+    game.load.image('bullet', 'assets/bullet.png');
+    game.load.image('missile', 'assets/missile.png');
   }
 
   function create() {
@@ -50,82 +59,65 @@ var appJs = (function  () {
     game.scale.pageAlignVertically = true;
     game.scale.refresh();
 
-    caribbean = game.add.graphics(0, 0); 
-    caribbean.beginFill(0x2c8af4);
-    caribbean.drawRect(worldBounds.xTopLeft, worldBounds.yTopLeft, worldBounds.xBottomRight, worldBounds.yBottomRight);
-    caribbean.endFill();
+    var sea = game.add.graphics(0, 0); 
+    sea.beginFill(0x2c8af4);
+    sea.drawRect(worldBounds.xTopLeft, worldBounds.yTopLeft, worldBounds.xBottomRight, worldBounds.yBottomRight);
+    sea.endFill();
 
     // Inicio el motor fisico
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    red = game.add.sprite(200, 500, 'red');
-    red.anchor.setTo(0.5, 0.5);
-    game.physics.enable(red, Phaser.Physics.ARCADE);
-    red.body.collideWorldBounds = true;
 
     // Genero la zona del caribe
     generateCaribbean();
     // Genero las islas
     generateIslands();
 
-    // Creo el submarino en una posicion aleatoria del caribe
+    // ------------------------------------------------------------------
+
+    //  A single bullet that the tank will fire
+    bullet = game.add.sprite(0, 0, 'bullet');
+    bullet.anchor.setTo(0.5, 0.5);
+    bullet.exists = false;
+    game.physics.arcade.enable(bullet);
+    bullet.body.checkWorldBounds = true;
+    bullet.outOfBoundsKill = true;
+
+    bulletButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    // bulletButton.onDown.add(fireBullet, game);
+
+    // ------------------------------------------------------------------
+
+    // Obtengo una posicion aleatoria dentro del caribe
     var rndX = game.rnd.between(caribeanZoneMin, caribeanZoneMax);
     var rndY = game.rnd.between(caribeanZoneMin, caribeanZoneMax);
-
+    // Creo el submarino
     submarine = game.add.sprite(100, 300, 'submarine');
     submarine.anchor.setTo(0.5, 0.5);
     game.physics.enable(submarine, Phaser.Physics.ARCADE);
     submarine.body.collideWorldBounds = true;
 
-    webSocketJs.setUser('submarine');
-    //webSocketJs.sendMessage(submarine.x, submarine.y, submarine.angle);
+    
 
-    // Pinta la tierra de new york
-    newYork = game.add.tileSprite(worldBounds.xTopLeft, worldBounds.yTopLeft, worldBounds.xBottomRight, 129, 'land');
-    game.physics.enable(newYork, Phaser.Physics.ARCADE);
-    newYork.body.setSize(worldBounds.xBottomRight, 135, 0, 0);
-    newYork.body.immovable = true;
-    // Dibujo la linea del borde
-    var newYorkLine = game.add.graphics(0, 0); 
-    newYorkLine.beginFill(0xE3F89A);
-    newYorkLine.drawRect(worldBounds.xTopLeft, worldBounds.yTopLeft + 129, worldBounds.xBottomRight, 6);
-    newYorkLine.endFill();
+    // Creo el barco
+    red = game.add.sprite(200, 500, 'red');
+    red.anchor.setTo(0.5, 0.5);
+    game.physics.enable(red, Phaser.Physics.ARCADE);
+    red.body.collideWorldBounds = true;
 
-    // Pinta la tierra de montevideo
-    montevideo = game.add.tileSprite(worldBounds.xTopLeft, worldBounds.yBottomRight - 129, worldBounds.xBottomRight, 129, 'land');
-    game.physics.enable(montevideo, Phaser.Physics.ARCADE);
-    montevideo.body.setSize(worldBounds.xBottomRight, 135, 0, -6);
-    montevideo.body.immovable = true;
-    // Dibujo la linea del borde
-    var montevideoLine = game.add.graphics(0, 0); 
-    montevideoLine.beginFill(0xE3F89A);
-    montevideoLine.drawRect(worldBounds.xTopLeft, worldBounds.yBottomRight - 135, worldBounds.xBottomRight, 6);
-    montevideoLine.endFill();
-    // Dibujo el puerto de montevideo
-    var portMontevideo = game.add.sprite(game.rnd.between(worldBounds.xTopLeft + 440, worldBounds.xBottomRight), worldBounds.yBottomRight, 'port');
-    portMontevideo.angle = 180;
+    // Genero los puertos con sus tierras
+    generatePorts();
 
-    port = game.add.sprite(game.rnd.between(worldBounds.xTopLeft, worldBounds.xBottomRight - 440), 0, 'port');
-    game.physics.enable(port, Phaser.Physics.ARCADE);
-    port.body.setSize(400, 60, 20, 135);
-    port.body.immovable = true;
-
+    // Seteo que la camara siga al submarino
     game.camera.follow(submarine);
     game.camera.focusOnXY(0, 0);
 
     cursors = game.input.keyboard.createCursorKeys();
 
-    //shadowTexture = game.add.bitmapData(game.width + 100, game.height + 100);
-    //lightSprite = game.add.image(0, 0, shadowTexture);
-    //lightSprite.blendMode = Phaser.blendModes.MULTIPLY;
-
+    // Genero una mascara y la aplico al world
+    // Esto determina la vision del barco
     mask = game.add.graphics(0, 0);
-    //  Shapes drawn to the Graphics object must be filled.
     mask.beginFill(0x000000);
-
-    //  Here we'll draw a circle
-    mask.drawCircle(0, 0, 400);
-
+    mask.drawCircle(0, 0, 800);
     game.world.mask = mask;
 
     game.input.activePointer.x = submarine.x;
@@ -151,38 +143,42 @@ var appJs = (function  () {
     //game.physics.arcade.collide([shoreMon, shoreNY], submarine);
     game.physics.arcade.collide([newYork, montevideo], submarine);
     game.physics.arcade.overlap(port, submarine, function() {
-      console.log("LLego!");
+      alert("LLego!");
+      submarine.kill();
+      // una vez que muere puede seguir al otro
+      game.camera.follow(red);
+      mask.destroy();
     });
 
     game.physics.arcade.collide(submarine, islands);
     game.physics.arcade.collide(red, submarine, function() {
-      red.body.velocity = { x: 0, y: 0 };
-      submarine.body.velocity = { x: 0, y: 0 };
-      console.log("Boom!");
+      //red.body.velocity = { x: 0, y: 0 };
+      //submarine.body.velocity = { x: 0, y: 0 };
+      alert("Boom!");
+      red.kill();
     });
     
     mask.x = submarine.body.x + 36;
     mask.y = submarine.body.y + 36;
-
-    //lightSprite.reset(game.camera.x, game.camera.y); 
     
-    //updateShadowTexture();
-    
-    //webSocketJs.sendMessage(submarine.x, submarine.y, submarine.angle);
+    // Manda la posicion al server
+    if (submarine.alive) {
+      webSocketJs.sendMessage('submarine', submarine.x, submarine.y, submarine.angle);
+    }
   
+    // Recibe la posición del oponente y la actualiza
     webSocketJs.setOnMessage(function (message) {
         var jsonMsg = JSON.parse(message.data);
-        console.log(jsonMsg);
+        // console.log(jsonMsg.user);
+        // console.log(jsonMsg.x);
+        // console.log(jsonMsg.y);
+        // console.log(jsonMsg.angle);
         if (jsonMsg.user == "red") {
-          red.body.x = jsonMsg.x;
-          red.body.y = jsonMsg.y;
-          //game.physics.arcade.accelerateToXY(red, jsonMsg.x, jsonMsg.y, 300);
+          red.x = jsonMsg.x;
+          red.y = jsonMsg.y;
+          red.angle = jsonMsg.angle;
         }
     });
-
-    // if (red.body.velocity > 0) {
-    //   red.body.velocity -= 5;
-    // }
 
     if (cursors.left.isDown)
     {
@@ -209,11 +205,25 @@ var appJs = (function  () {
       game.physics.arcade.velocityFromRotation(submarine.rotation, currentSpeed, submarine.body.velocity);
     }
 
+    if (bulletButton.isDown) {
+      fireBullet();
+    }
+    game.physics.arcade.collide(bullet, red, function() {
+      red.kill();
+      bullet.kill();
+      alert('Rojo hundido');
+    });
+    game.physics.arcade.collide([red, newYork, montevideo, islands], bullet, function() {
+      bullet.kill();
+    });
+
+
   }
 
   function render() {
     //game.debug.body(submarine);
-    //game.debug.body(port);
+    //game.debug.body(red);
+    game.debug.body(port);
   }
 
   var generateCaribbean = function() {
@@ -234,11 +244,61 @@ var appJs = (function  () {
     caribbean.endFill();
   }
 
+  var fireBullet = function() {
+    
+    if (game.time.now > nextFire) {
+
+      nextFire = game.time.now + fireRateBullet;
+      bullet.reset(submarine.x, submarine.y);
+      bullet.rotation = submarine.rotation;
+
+      // var gunPointing = Math.PI * 1.5;  // Izquierda
+      // var gunPointing = Math.PI * 0.5;  // Derecha
+      var gunPointing = 0;              // Frente
+
+      //  Disparo la bala considerando la direccion del barco
+      game.physics.arcade.velocityFromRotation(submarine.rotation + gunPointing, 500, bullet.body.velocity);
+      
+      var tween = game.add.tween(bullet).to(null, fireRateBullet, null, false, 0, 0, false);
+      
+      tween.onComplete.add(function() {
+        bullet.kill();
+      });
+      tween.start();
+    }
+
+  }
+
+  var fireMissile = function() {
+    
+    if (game.time.now > nextFire) {
+
+      nextFire = game.time.now + fireRate;
+      bullet.reset(submarine.x, submarine.y);
+      bullet.rotation = submarine.rotation;
+
+      // var gunPointing = Math.PI * 1.5;  // Izquierda
+      // var gunPointing = Math.PI * 0.5;  // Derecha
+      var gunPointing = 0;              // Frente
+
+      //  Disparo la bala considerando la direccion del barco
+      game.physics.arcade.velocityFromRotation(submarine.rotation + gunPointing, 350, bullet.body.velocity);
+      
+      var tween = game.add.tween(bullet).to(null, 500, null, false, 0, 0, false);
+      
+      tween.onComplete.add(function() {
+        bullet.kill();
+      });
+      tween.start();
+    }
+    
+  }
+
   var generateIslands = function() {
     islands = game.add.group();
 
-    // Seteo un valor random con la cantidad máxim
-    var numberOfIslands = game.rnd.integerInRange(20, 40);
+    // Seteo un valor random con la cantidad de islas
+    var numberOfIslands = game.rnd.integerInRange(15, 30);
     
     var i = 0, x, y, width, height, island;
 
@@ -261,4 +321,38 @@ var appJs = (function  () {
       islands.add(island);
     }
   }
+
+  var generatePorts = function() {
+    // Pinta la tierra de new york
+    newYork = game.add.tileSprite(worldBounds.xTopLeft, worldBounds.yTopLeft, worldBounds.xBottomRight, 129, 'land');
+    game.physics.enable(newYork, Phaser.Physics.ARCADE);
+    newYork.body.setSize(worldBounds.xBottomRight, 135, 0, 0);
+    newYork.body.immovable = true;
+    // Dibujo la linea del borde
+    var newYorkLine = game.add.graphics(0, 0); 
+    newYorkLine.beginFill(0xE3F89A);
+    newYorkLine.drawRect(worldBounds.xTopLeft, worldBounds.yTopLeft + 129, worldBounds.xBottomRight, 6);
+    newYorkLine.endFill();
+    // Dibujo el puerto de new york
+    port = game.add.sprite(game.rnd.between(worldBounds.xTopLeft, worldBounds.xBottomRight - 440), 0, 'port');
+    game.physics.enable(port, Phaser.Physics.ARCADE);
+    port.body.setSize(400, 60, 20, 135);
+    port.body.immovable = true;
+
+    // Pinta la tierra de montevideo
+    montevideo = game.add.tileSprite(worldBounds.xTopLeft, worldBounds.yBottomRight - 129, worldBounds.xBottomRight, 129, 'land');
+    game.physics.enable(montevideo, Phaser.Physics.ARCADE);
+    montevideo.body.setSize(worldBounds.xBottomRight, 135, 0, -6);
+    montevideo.body.immovable = true;
+    // Dibujo la linea del borde
+    var montevideoLine = game.add.graphics(0, 0); 
+    montevideoLine.beginFill(0xE3F89A);
+    montevideoLine.drawRect(worldBounds.xTopLeft, worldBounds.yBottomRight - 135, worldBounds.xBottomRight, 6);
+    montevideoLine.endFill();
+    // Dibujo el puerto de montevideo
+    var portMontevideo = game.add.sprite(game.rnd.between(worldBounds.xTopLeft + 440, worldBounds.xBottomRight), worldBounds.yBottomRight, 'port');
+    portMontevideo.angle = 180;
+  }
+
+
 })();
